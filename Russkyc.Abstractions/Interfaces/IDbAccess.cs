9ca92d68c4d70bc63a5a5a9b64e4476efde4a9ae
russkyc <jrcc1023@gmail.com>
2023-04-30 19:01:36 +0800

    feat: add more overloads to DbService, bump version to 1.0.6

--- Russkyc.Abstractions/Interfaces/IDbAccess.cs
+++ Russkyc.Abstractions/Interfaces/IDbAccess.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace Russkyc.Abstractions.Interfaces
 {
@@ -7,10 +8,16 @@ namespace Russkyc.Abstractions.Interfaces
     {
         bool Insert<T>(T item);
         bool Insert<T>(ICollection<T> item);
+        T Get<T>(Expression<Func<T, bool>> filter);
         T Get<T>(Func<T, bool> filter);
+        ICollection<T> GetAll<T>(Expression<Func<T, bool>> filter);
         ICollection<T> GetAll<T>(Func<T, bool> filter);
         ICollection<T> GetCollection<T>();
+        bool Update<T>(Expression<Func<T, bool>> filter, Action<T> action);
         bool Update<T>(Func<T, bool> filter, Action<T> action);
+        bool Update<T>(Expression<Func<T, bool>> filter, T item);
+        bool Update<T>(Func<T, bool> filter, T item);
+        bool Delete<T>(Expression<Func<T, bool>> filter);
         bool Delete<T>(Func<T, bool> filter);
     }
 }
